15ccdcc97475bd5ec0b040a8de1aed439a165caa
Kateryna Ueno <katerynaueno@gmail.com>
2021-08-19 17:05:23 +0100

    Commit

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,16 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+end
